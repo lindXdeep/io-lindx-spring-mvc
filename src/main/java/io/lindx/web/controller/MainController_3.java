16f82c0844bd0d9f6b56e9c5bdcff5a31c992baf
lindx <linder.igor@gmail.com>
2021-02-09 08:01:20 +0300

    @ResponseBody - like @RestController in MainController_1

---
+++ src/main/java/io/lindx/web/controller/MainController_3.java
@@ -0,0 +1,15 @@
+package io.lindx.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController_3 {
+
+    @GetMapping("/main1")
+    public @ResponseBody String printMainInfo(){
+        System.out.println("we are in main method 1");
+        return "Hello from RestController";
+    }
+}
