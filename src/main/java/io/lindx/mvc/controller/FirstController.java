f631ab048e5f7d09cdee2041709054ea4b4718ca
lindx <linder.igor@gmail.com>
2021-02-14 02:22:11 +0300

    Model2-calculator

--- src/main/java/io/lindx/mvc/controller/FirstController.java
+++ src/main/java/io/lindx/mvc/controller/FirstController.java
@@ -3,8 +3,10 @@ package io.lindx.mvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 @Controller
+// @RequestMapping("/first")
 public class FirstController {
     
     @GetMapping("/hello")
@@ -21,5 +23,31 @@ public class FirstController {
     @GetMapping("/bye")
     public String goodbye(){
         return "first/bye";
+    }
+
+    @GetMapping("/calc")
+    public String calculator(@RequestParam("a")      int a, 
+                             @RequestParam("b")      int b,
+                             @RequestParam("action") String action, 
+                             Model                   model) {
+
+        double result;
+
+        switch (action) {
+            case "multiplication":
+                result = a * b;             break;
+            case "division":
+                result = a / (double) b;    break;
+            case "subtraction":
+                result = a - b;             break;
+            case "addition":
+                result = a + b;             break;
+            default:
+                result = 0;                 break;
+        }
+
+        model.addAttribute("result", result);
+
+        return "first/calculator";
     }
 }
