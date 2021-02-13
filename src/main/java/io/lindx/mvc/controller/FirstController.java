4f0d96025252d3c7447d13e3ecf9fd2c0a13c00a
lindx <linder.igor@gmail.com>
2021-02-14 01:24:12 +0300

    GET-request HttpServletRequest

--- src/main/java/io/lindx/mvc/controller/FirstController.java
+++ src/main/java/io/lindx/mvc/controller/FirstController.java
@@ -1,14 +1,23 @@
 package io.lindx.mvc.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class FirstController {
     
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(HttpServletRequest request){
+
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+
+        System.out.println("hello: " + name + " " + surname);
+
         return "first/hello";
     }
+
     @GetMapping("/bye")
     public String goodbye(){
         return "first/bye";
